ad4154e2e4bbc627f0cb7bcb9230e24b7487634a
Daniel Azuaje <daniel.azuaje@manzares.com.ve>
2022-12-07 11:58:48 -0400

    added fonts, folders, and colors

---
+++ Spotify Challenge/Extensions/UIKit/UIColor+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Ext.swift
+//  Spotify Challenge
+//
+//  Created by Daniel Azuaje on 7/12/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static let accent = UIColor(red: 30/255, green: 215/255, blue: 96/255, alpha: 1.0)
+    
+    static let background = UIColor(red: 25/255, green: 20/255, blue: 20/255, alpha: 1.0)
+}
---
+++ Spotify Challenge/Scenes/New Releases/NewReleasesController.swift
@@ -0,0 +1,18 @@
+//
+//  NewReleasesController.swift
+//  Spotify Challenge
+//
+//  Created by Daniel Azuaje on 7/12/22.
+//
+
+import UIKit
+
+class NewReleasesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .background
+    }
+    
+}
