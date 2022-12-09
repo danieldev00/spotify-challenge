91825296ca37be0c04551329a3581d8f53c90936
Daniel Azuaje <daniel.azuaje@manzares.com.ve>
2022-12-09 17:48:37 -0400

    token

--- Spotify Challenge/Constants.swift
+++ Spotify Challenge/Constants.swift
@@ -10,6 +10,6 @@ import Foundation
 struct Constants {
     struct API {
         static let host = "https://api.spotify.com/v1"
-        static let auth = "BQC5NlCgIvboVv5Bg0mCkbbuAhqVhi0SZARj4S-wZuxnKj1EyRQY9-AwCZablA-Z14KiyHxC_ALe-Lx8jOmhYo3sjRX5Hi0nwliXT7gbECdq-xhVL7UQKcoFDOcp1z9Y6IOkJh11uJsuYuTLd_PuthnEOT0C-w5IJe4jxbue_Jve-QBBYoiG_YEl8EtBvuc"
+        static let auth = "Spotify Auth Token"
     }
 }
